b2511ff7aedc555a09209b62eea8709fd9166dbf
Piotr Zaniewski <piotrzan@gmail.com>
2024-07-03 11:49:39 +0200

    questions timer should start from 3
    Signed-off-by: Piotr Zaniewski <piotrzan@gmail.com>

--- __questions.sh
+++ __questions.sh
@@ -87,7 +87,7 @@ countdown_quiz() {
 	display_question "$question" "$answers" "$width" "$height" false
 	read -p "" </dev/tty
 
-	for i in {5..1}; do
+	for i in {3..1}; do
 		clear_and_position "$height" $((height / 2 - 4))
 
 		# Generate figlet text, put it in a box, center it, and colorize it
