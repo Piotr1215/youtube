534cd26dcbc8f13a1ee31ea6febeed85c3899732
Piotr Zaniewski <piotrzan@gmail.com>
2024-08-05 12:48:04 +0200

    nvim-plugins
    Signed-off-by: Piotr Zaniewski <piotrzan@gmail.com>

---
+++ nvim-plugins/greeter/greeter.lua
@@ -0,0 +1,8 @@
+local M = {}
+function M.greet(name)
+	print("Hi, " .. name .. ", consider subscribing")
+	return "Hi, " .. name .. ", consider subscribing"
+	-- return "Hi, " .. name .. " consider subscribing"
+end
+
+return M
---
+++ nvim-plugins/intro.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+title="Developing Neovim Plugins"
+
+if command -v figlet &>/dev/null && command -v boxes &>/dev/null; then
+	echo "$title" | figlet -f big | boxes -d peek
+else
+	echo "$title"
+fi
